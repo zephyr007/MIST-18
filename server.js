63952cd5e1b48c7edb7e4c44488b901a6062b3f6
nitin dhemiwal <dhemiwal.nitin@gmail.com>
2018-07-12 21:36:58 +0530

    adding pages

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const ip = process.env.IP || '127.0.0.1';
-var firebase = require("firebase");
+const  firebase = require('firebase');
+const firebaseui = require('firebaseui');
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAkNXb7dJeu-IR3BvSt1lN3samo-CdAKsM",
@@ -12,13 +13,30 @@ var config = {
     messagingSenderId: "627563380427"
 };
 
+firebase.initializeApp(config);
+var id_token = googleUser.getAuthResponse().id_token;
+
+// Build Firebase credential with the Google ID token.
+var credential = firebase.auth.GoogleAuthProvider.credential(id_token);
+
+// Sign in with credential from the Google user.
+firebase.auth().signInAndRetrieveDataWithCredential(credential).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+});
 
 app = express();
 
 app.get('/',function (req, res) {
-    res.send("we are live now")
+    res.sendFile('login.html')
 });
-firebase.initializeApp(config);
+
 
 app.listen(port,ip,function () {
     console.log("listening to port : "+ port );
